4cf4146150a0976b8e8b561fb98ea6e0047b1019
RiveSunder <RiveSunder@protonmail.com>
2022-04-27 17:10:04 -0600

    troubleshooting mybinder 2

--- bokehserverextension.py
+++ bokehserverextension.py
@@ -2,4 +2,4 @@ from subprocess import Popen
 
 def load_jupyter_server_extension(nbapp):
     """serve the bokeh-app directory with bokeh server"""
-    Popen(["bokeh", "serve", "gecco_bokeh", "--allow-websocket-origin=*"])
+    Popen(["bokeh", "serve", "bokeh-app", "--allow-websocket-origin=*"])
