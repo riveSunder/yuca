9ee08ecad2a02d78cc81788f656da3db9deccb6d
RiveSunder <RiveSunder@protonmail.com>
2022-04-28 09:51:33 -0600

    update names (snake case for bokeh_server_extension.py, bokeh-app -> gecco_bokeh_app)

--- bokeh_server_extension.py
+++ bokeh_server_extension.py
@@ -2,4 +2,4 @@ from subprocess import Popen
 
 def load_jupyter_server_extension(nbapp):
     """serve the bokeh-app directory with bokeh server"""
-    Popen(["bokeh", "serve", "bokeh-app", "--allow-websocket-origin=*"])
+    Popen(["bokeh", "serve", "gecco_bokeh_app", "--allow-websocket-origin=*"])
