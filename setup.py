ac4d9960b10c10f193eb012a19639f40f88340df
RiveSunder <RiveSunder@protonmail.com>
2022-05-05 10:22:35 -0600

    bring in requirements.txt and setup.py from gecco pages

--- setup.py
+++ setup.py
@@ -5,7 +5,16 @@ setup(\
         name="yuca", \
         packages=["yuca"], \
         version = "0.0", \
-        description = "Your Universal Cellular Automata" \
+        description = "Your Universal Cellular Automata", \
+        install_requires=[\
+                        "bokeh==2.4.1",\
+                        "jupyter==1.0.0",\
+                        "notebook==6.3.0",\
+                        "numpy==1.18.4",\
+                        "torch==1.5.1",\
+                        "scikit-image==0.17.2",\
+                        "jupyter_server_proxy",\
+                        "matplotlib==3.3.3"]\
     )
 
 
