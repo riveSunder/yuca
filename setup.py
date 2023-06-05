5c5d1154b72e1e387221b94fe71f45c1d6ade9e0
Your Name <you@example.com>
2023-06-05 13:58:27 -0600

    update dependencies in setup.py to prevent 'forbidden' error in jupyter notebooks

--- setup.py
+++ setup.py
@@ -9,27 +9,19 @@ setup(\
         install_requires=[\
                         "bokeh==2.4.1",\
                         "coverage==7.0.3",\
-                        "jupyter==1.0.0",\
-                        "notebook>=6.4.12",\
                         "numpy==1.22.0",\
                         "torch==2.0.1",\
-                        "cycler==0.11.0",\
-                        "decorator==4.4.2",\
                         "future==0.18.3",\
                         "imageio==2.14.1",\
-                        "kiwisolver==1.3.1",\
                         "matplotlib==3.3.4",\
-                        "networkx==2.5.1",\
                         "Pillow==9.3.0",\
                         "pyparsing==3.0.7",\
                         "python-dateutil==2.8.2",\
-                        "PyWavelets==1.1.1",\
                         "scikit-image==0.17.2",\
                         "scipy==1.5",\
                         "six==1.16.0",\
                         "tifffile==2020.9.3",\
-                        "scikit-image==0.17.2",\
-                        "jupyter_server_proxy"]\
+                        "scikit-image==0.17.2"]\
     )
 
 
