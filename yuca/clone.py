08ae38dde9db8fe034871110ee5e54facfa526ab
Your Name <you@example.com>
2023-06-08 12:51:43 -0600

    update cloning method clone.py
    clone multiple rule sets from a list and other updates

--- yuca/clone.py
+++ yuca/clone.py
@@ -11,22 +11,17 @@ from yuca.utils import query_kwargs
 from yuca.ca.neural import NCA
 from yuca.ca.continuous import CCA
 
-def clone_from_ca(ca_config, **kwargs):
+def clone_from_ca(**kwargs):
     """
     clone a continuous CA rule set specified in the filepath ca_config
     """
 
-    ca = CCA()
     file_directory = os.path.abspath(__file__).split("/")
     root_directory = os.path.join(*file_directory[:-2])
     default_directory = os.path.join("/", root_directory, "ca_configs")
     log_directory = os.path.join("/", root_directory, "logs")
 
-    ca.restore_config(ca_config)
 
-    internal_channels = ca.internal_channels
-    external_channels = ca.external_channels
-    hidden_channels = query_kwargs("hidden_channels", 32, **kwargs)
     max_hidden_channels = query_kwargs("max_hidden_channels", 2048, **kwargs)
     error_threshold = query_kwargs("error_threshold", 1e-2, **kwargs)
     max_iterations = query_kwargs("max_iterations", 10, **kwargs)
@@ -38,82 +33,109 @@ def clone_from_ca(ca_config, **kwargs):
     save_name = query_kwargs("save_name", "default_nca.pt", **kwargs)
     save_path = os.path.join(log_directory, save_name)
 
-    relative_error = float("Inf")
-    best_error = float("Inf")
+
     best_state_dict = None
     
-    iteration = 0
+    ca_configs = query_kwargs("ca_config", "orbium", **kwargs)
+    if type(ca_configs) == str:
+        ca_configs = [ca_configs]
 
-    t0 = time.time()
-    while best_error > error_threshold and iteration < max_iterations:
+    for ca_config in ca_configs:
+        hidden_channels = query_kwargs("hidden_channels", 32, **kwargs)
 
-        iteration += 1
-        nca = NCA(internal_channels=internal_channels,\
-                external_channels=external_channels,\
-                hidden_channels=hidden_channels)
+        ca = CCA()
+        ca.restore_config(ca_config)
+        internal_channels = ca.internal_channels
+        external_channels = ca.external_channels
 
-        optimizer = torch.optim.Adam(nca.parameters(), lr=learning_rate)
-        smooth_loss = None
-        # loss_alpha used for exponential averaging
-        loss_alpha = 0.99
-        for step in range(max_steps):
-            
-            optimizer.zero_grad()
+        iteration = 0
+        best_error = float("Inf")
 
-            x = torch.rand(batch_size, external_channels, 256, 256)
+        t0 = time.time()
+        while iteration < max_iterations:
+            iteration_best_error = float("Inf")
+            relative_error = float("Inf")
 
-            target = ca.update_helper(x)
-            predicted = nca.update_helper(x)
-            loss = F.mse_loss(predicted, target)
+            nca = NCA(internal_channels=internal_channels,\
+                    external_channels=external_channels,\
+                    hidden_channels=hidden_channels)
 
-            loss.backward()
-            optimizer.step()
+            optimizer = torch.optim.Adam(nca.parameters(), lr=learning_rate)
+            smooth_loss = None
+            # loss_alpha used for exponential averaging
+            loss_alpha = 0.99
+            for step in range(max_steps):
+                
+                optimizer.zero_grad()
 
-            if smooth_loss is None:
-                smooth_loss = loss.detach()
-            else:
-                smooth_loss = smooth_loss * loss_alpha + (1-loss_alpha) * loss.detach()
+                x = torch.rand(batch_size, external_channels, 256, 256)
+                x *= (1.0 * torch.rand(batch_size, external_channels, 256, 256) > 0.75)
 
-            relative_error = torch.abs(predicted - target).mean()
+                target = ca.update_helper(x)
+                predicted = nca.update_helper(x)
+                loss = F.mse_loss(predicted, target)
 
-            if relative_error < best_error:
-                best_error = relative_error
+                loss.backward()
+                optimizer.step()
 
-                best_state_dict = nca.state_dict()
+                if smooth_loss is None:
+                    smooth_loss = loss.detach()
+                else:
+                    smooth_loss = smooth_loss * loss_alpha + (1-loss_alpha) * loss.detach()
 
-                if verbose:
-                    msg = f"new best relative error at step {step} "\
-                            f"= {best_error:.3e}"
+                relative_error = (torch.abs(predicted - target)/(target.abs()+1e-9)).mean()
+
+                if relative_error < iteration_best_error:
+                    iteration_best_error = relative_error
+
+                    if relative_error < best_error:
+                        best_error = relative_error
+                        best_state_dict = nca.state_dict()
+
+                    if verbose:
+                        msg = f"iteration {iteration} new best relative error at step {step} "\
+                                f"= {iteration_best_error:.3e}, best overall = {best_error:.3e}\n"
+                        print(msg)
+
+                    if iteration_best_error < error_threshold:
+                        break
+
+
+                if step % display_every == 0:
+                    t1 = time.time()
+                    time_elapsed = t1 - t0
+                    msg = f"iteration {iteration} with {hidden_channels} ch_h "\
+                            f"step {step} loss; {smooth_loss:.4e}"\
+                            f" time elapsed: {time_elapsed:.2f}"
                     print(msg)
 
+            hidden_channels = min([max_hidden_channels, int(hidden_channels*1.25)])
+            iteration += 1
+            
 
-            if step % display_every == 0:
-                t1 = time.time()
-                time_elapsed = t1 - t0
-                msg = f"iteration {iteration} step {step} loss; {smooth_loss:.4e}"\
-                        f" time elapsed: {time_elapsed:.2f}"
-                print(msg)
+        msg = f"attempted to clone {ca_config} to nca\n"
+        msg += f"best nca after {iteration} iterations of max {max_steps} steps \n"
 
-        hidden_channels = min([max_hidden_channels, int(hidden_channels*1.25)])
-        
+        if best_error < error_threshold:
+            msg += f"\tmeets error threshold {error_threshold:.3e} with loss {best_error:.3e}\n"
+        else:
+            msg += f"\tdid not meet error threshold {error_threshold:.3e} with loss {best_error:.3e}\n"
 
-    msg = f"best nca after {iteration} iterations of max {max_steps} steps"
-    if best_error < error_threshold:
-        msg += f"     meets error threshold {error_threshold:.3e} with loss {best_error:.3e} \n"
-    else:
-        msg += f"     did not meet error threshold {error_threshold:.3e} with loss {best_error:.3e} \n"
+        if "ca_config" in save_path:
+            config_name = os.path.splitext(ca_config)[0] 
+            save_path = save_path.replace("ca_config", config_name)
 
-    msg += f"saving to {save_path}"
-    print(msg)
+        msg += f"saving to {save_path}"
+        print(msg)
 
-    torch.save(best_state_dict, save_path)
+        torch.save(best_state_dict, save_path)
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
 
-    parser.add_argument("-cc", "--ca_config", type=str,\
-        default="orbium.npy",\
+    parser.add_argument("-cc", "--ca_config", type=str, nargs="+",\
+        default=["orbium.npy"],\
         help="CA config filepath (or filename) to clone NCA from. Default: orbium.npy")
     parser.add_argument("-e", "--error_threshold", type=float,\
         default=1e-3,\
@@ -127,13 +149,13 @@ if __name__ == "__main__":
     parser.add_argument("-o", "--save_name", type=str,\
         default="default_nca.pt",\
         help="NCA filepath to save results")
-
-
+    parser.add_argument("-v", "--verbose", type=int,\
+        default=0,\
+        help="0- not verbose 1- verbose")
 
 
     args = parser.parse_args()
 
-    ca_config = args.ca_config
     kwargs = dict(args._get_kwargs())
 
     clone_from_ca(**kwargs)
