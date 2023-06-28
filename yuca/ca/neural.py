f4e663c0b9d195724bbb3960e7193761968ef12d
Your Name <you@example.com>
2023-06-28 15:01:12 -0600

    add catch for mis-specified/not allowed activation functions in kwargs to NCA

--- yuca/ca/neural.py
+++ yuca/ca/neural.py
@@ -65,6 +65,11 @@ class NCA(CA):
 
         if type(self.act) == str and self.act in activation_dict.keys():
             self.act = activation_dict[self.act]
+        else type(self.act) == str:
+            # this warning is more specific than all scenarios that might be captured here
+            print(f"Warning, {self.act} not in allowed activation function dictionary"}
+            print(f"Defaulting to Gaussian activation function")
+            self.act = Gaussian
 
         self.default_init()
         self.reset()
