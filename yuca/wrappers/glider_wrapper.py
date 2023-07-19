812ca8cdae7cad6bbaeca5e2433d1924fc39e735
Your Name <38316715+riveSunder@users.noreply.github.com>
2023-07-19 15:24:11 -0600

    revert changes to glider_wrapper per review comments
    the changes don't actually facilitate evolving gliders in the gray-scott system

--- yuca/wrappers/glider_wrapper.py
+++ yuca/wrappers/glider_wrapper.py
@@ -92,19 +92,13 @@ class GliderWrapper():
        
             action = self.ca(action)
 
-            if action.shape[1] == 2:
-                # indicates rxn diffusion system
-                action_of_interest = 1.0 *action[:,0:1,:,:]
-            else:
-                action_of_interest = 1.0 *action
-
-            mean_grid = action_of_interest.mean()
-            max_grid = action_of_interest.max()
+            mean_grid = action.mean()
+            max_grid = action.max()
 
             if step % (score_every) == 0:# or step == (self.ca_steps - 1):
                 
 
-                y_displacement_1 = (self.y_grid * action_of_interest[0:1]).sum() / (eps + action.sum())
+                y_displacement_1 = (self.y_grid * action[0:1]).sum() / (eps + action.sum())
 
 
                 # penalize growth/decay, i.e. incentivize morphological homeostasis
@@ -127,8 +121,8 @@ class GliderWrapper():
                 y_displacement_0 = y_displacement_1.clone()
 
 
-            dgrid = action_of_interest - old_grid
-            old_grid = 1.0  * action_of_interest
+            dgrid = action - old_grid
+            old_grid = 1.0  * action
 
             if mean_grid == 0.0 or dgrid.max() <= 0.00001:
                 break
