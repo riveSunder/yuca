e68e43ca580f14fb38e78467e4de576e2ef5d373
Your Name <38316715+riveSunder@users.noreply.github.com>
2023-07-20 17:53:33 -0600

    add change_kernel_radius to reaction diffusion
    currently gliders are not stable with kernel radius != 1

--- yuca/ca/reaction_diffusion.py
+++ yuca/ca/reaction_diffusion.py
@@ -37,6 +37,14 @@ class RxnDfn(CA):
         self.default_init()
         self.reset()
 
+    def change_kernel_radius(self, radius):
+
+        self.neighborhood_kernel_config["radius"] = radius
+        nbhd_kernel = get_kernel(self.neighborhood_kernel_config)
+        self.add_neighborhood_kernel(nbhd_kernel)
+        self.kernel_radius = self.neighborhood_kernel_config["radius"]
+        self.initialize_neighborhood_layer()
+
     def load_config(self, config):
 
         self.config = config
